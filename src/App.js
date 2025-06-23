6771ad44866d40840aba2507be010ce11ca11903
Pabasara Samarakoon <pabasara96samarakoon@gmail.com>
2025-06-23 13:15:37 +0530

    Route for request from -paba

--- src/App.js
+++ src/App.js
@@ -2,9 +2,15 @@ import User from './User/User';
 import './App.css';
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 import 'bootstrap/dist/css/bootstrap.min.css';
+
 import EngineerDashboard from './Position/EngineerDashboard';
 import Register from './api/Register';
 import Login from './api/Login';
+
+//requestform
+import RequestForm from './Request/RequestForm';
+import Notification from './Request/Notification';
+
 
 function App() {
   return (
@@ -12,9 +18,14 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/User" element={<User />} />
+
           <Route path="/Engineer" element={<EngineerDashboard />} />
           <Route path="/register" element={<Register />} />
           <Route path="/" element={<Login />} />
+
+
+          <Route path="/RequestForm" element={<RequestForm />} />
+          <Route path="/Notification" element={<Notification />} />
 
         </Routes>
       </BrowserRouter>
