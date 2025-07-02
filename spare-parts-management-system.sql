bfc40c749987a0e6b3f67a28013ad9a0a0bd9dde
MohomedImshan <152673385+MohomedImshan@users.noreply.github.com>
2025-07-02 21:15:05 +0530

    Add files via upload

---
+++ spare-parts-management-system.sql
@@ -0,0 +1,95 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 02, 2025 at 05:43 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `spare-parts-management-system`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `spare_parts_tbl`
+--
+
+CREATE TABLE `spare_parts_tbl` (
+  `id` int(11) NOT NULL,
+  `department` varchar(100) NOT NULL,
+  `type` varchar(50) NOT NULL,
+  `item_name` varchar(100) NOT NULL,
+  `quantity` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `empNum` int(11) NOT NULL,
+  `userName` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `position` varchar(50) NOT NULL DEFAULT 'Technician',
+  `status` varchar(50) NOT NULL DEFAULT 'Disabled'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`empNum`, `userName`, `email`, `password`, `position`, `status`) VALUES
+(1, 'Engineer1', 'eng1@gmail.com', '$2b$10$KewVMOxChFTyClDndhARjOpqPJLhe0zYtwpeR.Tyqnh7tvs81pri.', 'Engineer', 'Disabled');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `spare_parts_tbl`
+--
+ALTER TABLE `spare_parts_tbl`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`empNum`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `spare_parts_tbl`
+--
+ALTER TABLE `spare_parts_tbl`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `empNum` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
